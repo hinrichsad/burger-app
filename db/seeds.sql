086c58c61e9287c8a6b24a67945a813b930075ab
Andy Hinrichs <hinrichsad@gmail.com>
2021-03-05 15:08:04 -0500

    basic skeleton in handlebars, server and database set up and connected

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) values ('Boiled Sardines and Okra burger', false);
+
+INSERT INTO burgers (burger_name, devoured) values ('Pickles. Just pickles on bread', false);
+
+INSERT INTO burgers (burger_name, devoured) values ('Bacon and Avacado Burger', true);
---
+++ models/burger.js
@@ -0,0 +1,20 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require('../config/orm.js');
+
+const burger = {
+    // returns all burgers
+    all(cb) {
+        orm.selectAll('burgers', (res) => cb(res));
+    },
+    //creates a burger
+    create(cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, (res) => cb(res));
+    },
+    //update function
+    update(objColVals, condition, cb) {
+        orm.updateOne('burgers', objColVals, condition, (res) => cb(res));
+    },
+};
+
+// Export the database functions for the controller
+module.exports = burger;
